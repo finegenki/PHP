4a93de525c25ca517fc0acb1adbb4283fbc048c6
finegenki <finegenki0907@gmail.com>
2023-08-11 08:11:29 +0900

    3-2課題再提出

--- 3-2/Kansu.php
+++ 3-2/Kansu.php
@@ -1,7 +1,7 @@
 <?php
  //商品の税込価格を計算しましょう
   //①税率を定数TAXで作成しましょう。消費税は10%とします。
-  $TAX = 0.1;
+  const TAX = 0.1;
 
   // //②商品の情報を連想配列に入れましょう。
     $products = ["鉛筆" =>"100" , "消しゴム" =>"150", "物差し" =>"500" ];
@@ -9,9 +9,7 @@
   // //③税率を計算する関数を用意します。
   // //引数には値段を受け取り、税込価格を返答します。
     function taxIncluded ($product){
-        global $TAX;
-        $taxIncluded = $product + $product * $TAX; 
-        return $taxIncluded;
+        return $product + $product * TAX; 
     }
 
   // //④繰り返し文を使って画面に指定の文字を表示しましょう！
